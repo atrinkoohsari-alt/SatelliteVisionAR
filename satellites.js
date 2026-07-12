317f06d09ba064281f362b81632cb4f857564116
atrinkoohsari-alt <atrin.koohsari@gmail.com>
2026-07-12 22:31:59 +0330

    Create satellites.js

---
+++ satellites.js
@@ -0,0 +1,27 @@
+const satellites = [
+
+{
+    id: 1,
+    name: "Yahsat 52.5E",
+    longitude: 52.5
+},
+
+{
+    id: 2,
+    name: "TurkmenAlem 52E",
+    longitude: 52.0
+},
+
+{
+    id: 3,
+    name: "Hotbird 13E",
+    longitude: 13.0
+},
+
+{
+    id: 4,
+    name: "Astra 19.2E",
+    longitude: 19.2
+}
+
+];
