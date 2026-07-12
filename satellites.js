2cbee743026198c1e725ca7d83eebe94a0d422bd
atrinkoohsari-alt <atrin.koohsari@gmail.com>
2026-07-12 22:37:54 +0330

    Update satellites.js

--- satellites.js
+++ satellites.js
@@ -1,27 +1,52 @@
 const satellites = [
 
-{
-    id: 1,
-    name: "Yahsat 52.5E",
-    longitude: 52.5
-},
+    {
+        id: 1,
+        name: "Yahsat 52.5E",
+        longitude: 52.5
+    },
 
-{
-    id: 2,
-    name: "TurkmenAlem 52E",
-    longitude: 52.0
-},
+    {
+        id: 2,
+        name: "TurkmenAlem 52E",
+        longitude: 52.0
+    },
 
-{
-    id: 3,
-    name: "Hotbird 13E",
-    longitude: 13.0
-},
+    {
+        id: 3,
+        name: "Hotbird 13E",
+        longitude: 13.0
+    },
 
-{
-    id: 4,
-    name: "Astra 19.2E",
-    longitude: 19.2
-}
+    {
+        id: 4,
+        name: "Astra 19.2E",
+        longitude: 19.2
+    },
+
+    {
+        id: 5,
+        name: "Nilesat 7W",
+        longitude: -7.0
+    },
+
+    {
+        id: 6,
+        name: "Eutelsat 7E",
+        longitude: 7.0
+    },
+
+    {
+        id: 7,
+        name: "TürkmenÄlem/MonacoSat",
+        longitude: 52.0
+    },
+
+    {
+        id: 8,
+        name: "Starlink",
+        longitude: null,
+        type: "starlink"
+    }
 
 ];
