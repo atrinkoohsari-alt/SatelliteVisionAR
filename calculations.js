ad747802d31ad846fecb4d197a2740f87ead449d
atrinkoohsari-alt <atrin.koohsari@gmail.com>
2026-07-19 18:34:14 +0330

    Update calculations.js

--- calculations.js
+++ calculations.js
@@ -1,17 +1,27 @@
+function toRadians(degrees){
+    return degrees * Math.PI / 180;
+}
+
+function toDegrees(radians){
+    return radians * 180 / Math.PI;
+}
+
 function calculateAzimuth(latitude, longitude, satelliteLongitude){
 
-    return 180 + (satelliteLongitude - longitude);
+    const lat = toRadians(latitude);
+    const dLon = toRadians(satelliteLongitude - longitude);
 
-}
+    let azimuth = Math.atan(
+        Math.tan(dLon) / Math.sin(lat)
+    );
 
-function calculateElevation(latitude, longitude, satelliteLongitude){
+    azimuth = toDegrees(azimuth);
 
-    return 0;
+    if(dLon > 0){
+        azimuth = 180 - azimuth;
+    }else{
+        azimuth = 180 + Math.abs(azimuth);
+    }
 
-}
-
-function calculateSkew(latitude, longitude, satelliteLongitude){
-
-    return 0;
-
+    return azimuth;
 }
